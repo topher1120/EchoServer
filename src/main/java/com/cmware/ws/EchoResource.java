168852b7c17d355b17370bd11d87f1be4694ea24
cmarks <cmarks@cmwareonline.com>
2012-10-04 22:50:16 -0600

    Add JMX monitoring to the resource.

--- src/main/java/com/cmware/ws/EchoResource.java
+++ src/main/java/com/cmware/ws/EchoResource.java
@@ -1,21 +1,43 @@
 package com.cmware.ws;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 @Path("/echo")
+@ManagedResource(objectName = "bean:name=echoResource", log = true, description = "The Echo Resource")
 public class EchoResource {
+
+    private AtomicInteger currentRequests = new AtomicInteger(0);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String echo(@QueryParam("value") String value) {
+        currentRequests.incrementAndGet();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+
+        }
+        currentRequests.decrementAndGet();
         return value;
+    }
+
+    @ManagedAttribute(description = "The current number of requests for this service resource.")
+    public Integer getCurrentRequests() {
+        return currentRequests.get();
     }
 
 }
