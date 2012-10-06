5c3da6e8bece80eee273951f59bdb8ffe16843d1
cmarks <cmarks@cmwareonline.com>
2012-10-05 23:27:28 -0600

    got the app ready for Heroku (I think)

---
+++ src/main/java/com/cmware/ws/HerokuStart.java
@@ -0,0 +1,37 @@
+package com.cmware.ws;
+
+
+/**
+ * Class for Heroku to start.
+ */
+public class HerokuStart {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        // final AnnotationConfigWebApplicationContext applicationContext = new
+        // AnnotationConfigWebApplicationContext();
+        // // applicationContext.register(WebConfig.class);
+        // applicationContext.setConfigLocation("classpath:/applicationContext.xml");
+        //
+        // final ServletHolder servletHolder = new ServletHolder(new
+        // SpringServlet());
+        // final ServletContextHandler context = new ServletContextHandler();
+        // context.setContextPath("/");
+        // context.addServlet(servletHolder, "/*");
+        //
+        // String webPort = System.getenv("PORT");
+        // if (webPort == null || webPort.isEmpty()) {
+        // webPort = "8080";
+        // }
+        //
+        // final Server server = new Server(Integer.valueOf(webPort));
+        //
+        // server.setHandler(context);
+        //
+        // server.start();
+        // server.join();
+    }
+
+}
