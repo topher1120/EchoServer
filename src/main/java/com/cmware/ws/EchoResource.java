32a4f2d7fb0058f317dab748165c78d57b33bd87
cmarks <cmarks@cmwareonline.com>
2012-10-06 21:42:28 -0600

    Got Logback working with a logback.xml file.  Also, commented out the code for the Heroku Tomcat start file since I removed the dependencies from Gradle.

--- src/main/java/com/cmware/ws/EchoResource.java
+++ src/main/java/com/cmware/ws/EchoResource.java
@@ -8,6 +8,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
@@ -20,11 +22,14 @@ import org.springframework.stereotype.Component;
 @ManagedResource(objectName = "bean:name=echoResource", log = true, description = "The Echo Resource")
 public class EchoResource {
 
+    private static Logger LOG = LoggerFactory.getLogger("c.c.w.EchoResource");
+
     private AtomicInteger currentRequests = new AtomicInteger(0);
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String echo(@QueryParam("value") String value) {
+    @Produces({ MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON })
+    public Object echo(@QueryParam("value") String value) {
+        LOG.debug("echoing request, value: {}", value);
         currentRequests.incrementAndGet();
         // for testing, delay response for 1 second.
         try {
