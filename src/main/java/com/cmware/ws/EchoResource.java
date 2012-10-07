3e6dda15d64b5fa743a3559ceb6721b2326dd4c7
cmarks <cmarks@cmwareonline.com>
2012-10-07 15:54:01 -0600

    Added a simple JSON response endpoint to the app.

--- src/main/java/com/cmware/ws/EchoResource.java
+++ src/main/java/com/cmware/ws/EchoResource.java
@@ -6,8 +6,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -27,10 +31,11 @@ public class EchoResource {
     private AtomicInteger currentRequests = new AtomicInteger(0);
 
     @GET
-    @Produces({ MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.TEXT_PLAIN })
     public Object echo(@QueryParam("value") String value) {
-        LOG.debug("echoing request, value: {}", value);
         currentRequests.incrementAndGet();
+        LOG.debug("echoing request, value: {}", value);
+
         // for testing, delay response for 1 second.
         try {
             Thread.sleep(1000);
@@ -39,6 +44,25 @@ public class EchoResource {
         }
         currentRequests.decrementAndGet();
         return value;
+    }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject echoJson(@QueryParam("value") String value) {
+        currentRequests.incrementAndGet();
+        LOG.debug("json echo request, value: {}", value);
+        try {
+            JSONObject jsonValue = new JSONObject();
+
+            jsonValue.put("value", value);
+            return jsonValue;
+        } catch (JSONException e) {
+            LOG.error("Error creating JSON: {}", e.getMessage());
+            throw new WebApplicationException(Response.serverError().entity("Error creating JSON response").build());
+        } finally {
+            currentRequests.decrementAndGet();
+        }
     }
 
     @ManagedAttribute(description = "The current number of requests for this service resource.")
